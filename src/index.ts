656ee80aecd3e8c660bc6e01f82993c052e87897
Bryce Carr <bcarr@deputy.com>
2019-11-04 16:56:58 +1100

    fix: https://github.com/JustinBeckwith/retry-axios/issues/1 (credit: @opyate)

--- src/index.ts
+++ src/index.ts
@@ -86,6 +86,11 @@ function onFulfilled(res: AxiosResponse) {
   return res;
 }
 
+// tslint:disable-next-line no-any
+const values = (objOrArray: {[key: string]: any}) => {
+  return Object.keys(objOrArray).map((key: string) => objOrArray[key]);
+};
+
 function onError(err: AxiosError) {
   const config = (err.config as RaxConfig).raxConfig || {};
   config.currentRetryAttempt = config.currentRetryAttempt || 0;
@@ -93,17 +98,13 @@ function onError(err: AxiosError) {
     config.retry === undefined || config.retry === null ? 3 : config.retry;
   config.retryDelay = config.retryDelay || 100;
   config.instance = config.instance || axios;
-  config.httpMethodsToRetry = config.httpMethodsToRetry || [
-    'GET',
-    'HEAD',
-    'PUT',
-    'OPTIONS',
-    'DELETE',
-  ];
-  config.noResponseRetries =
-    config.noResponseRetries === undefined || config.noResponseRetries === null
-      ? 2
-      : config.noResponseRetries;
+  config.httpMethodsToRetry = values(
+      config.httpMethodsToRetry || ['GET', 'HEAD', 'PUT', 'OPTIONS', 'DELETE']);
+  config.noResponseRetries = (config.noResponseRetries === undefined ||
+      config.noResponseRetries === null) ?
+      2 :
+      config.noResponseRetries;
+
 
   // If this wasn't in the list of status codes where we want
   // to automatically retry, return.
@@ -119,7 +120,8 @@ function onError(err: AxiosError) {
     [429, 429],
     [500, 599],
   ];
-  config.statusCodesToRetry = config.statusCodesToRetry || retryRanges;
+  config.statusCodesToRetry =
+      values(config.statusCodesToRetry || retryRanges).map(values);
 
   // Put the config back into the err
   (err.config as RaxConfig).raxConfig = config;
