6ba85b63b32eed6615a277757937e3cc054fbd8c
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2021-08-16 12:04:21 +0200

    tests: test delay for linear, exp strategies

--- test/index.ts
+++ test/index.ts
@@ -149,7 +149,84 @@ describe('retry-axios', () => {
     );
   });
 
-    process.stdout.write(`\n\n ${requesttimes}\n\n`);
+  it('should have non-zero delay between first and second attempt, linear backoff', async () => {
+    const requesttimes: bigint[] = [];
+    const scopes = [
+      nock(url)
+        .get('/')
+        .reply((_, __) => {
+          requesttimes.push(process.hrtime.bigint());
+          return [500, 'foo'];
+        }),
+      nock(url)
+        .get('/')
+        .reply((_, __) => {
+          requesttimes.push(process.hrtime.bigint());
+          return [200, 'bar'];
+        }),
+    ];
+
+    interceptorId = rax.attach();
+    const res = await axios({
+      url,
+      raxConfig: {
+        backoffType: 'linear',
+      },
+    });
+
+    // Confirm that first retry did yield 200 OK with expected body
+    assert.strictEqual(res.data, 'bar');
+    scopes.forEach(s => s.done());
+
+    assert.strictEqual(requesttimes.length, 2);
+    const delayInSeconds = Number(requesttimes[1] - requesttimes[0]) / 10 ** 9;
+
+    // The default delay between the first two attempts using the
+    // linear backoff strategy is 1000 ms. Test with tolerance.
+    assert.strict(
+      1.1 > delayInSeconds && delayInSeconds > 1.0,
+      `unexpected delay: ${delayInSeconds.toFixed(3)} s`
+    );
+  });
+
+  it('should have non-zero delay between first and second attempt, exp backoff', async () => {
+    const requesttimes: bigint[] = [];
+    const scopes = [
+      nock(url)
+        .get('/')
+        .reply((_, __) => {
+          requesttimes.push(process.hrtime.bigint());
+          return [500, 'foo'];
+        }),
+      nock(url)
+        .get('/')
+        .reply((_, __) => {
+          requesttimes.push(process.hrtime.bigint());
+          return [200, 'bar'];
+        }),
+    ];
+
+    interceptorId = rax.attach();
+    const res = await axios({
+      url,
+      raxConfig: {
+        backoffType: 'exponential',
+      },
+    });
+
+    // Confirm that first retry did yield 200 OK with expected body
+    assert.strictEqual(res.data, 'bar');
+    scopes.forEach(s => s.done());
+
+    assert.strictEqual(requesttimes.length, 2);
+    const delayInSeconds = Number(requesttimes[1] - requesttimes[0]) / 10 ** 9;
+
+    // The default delay between attempts using the
+    // exp backoff strategy is 500 ms. Test with tolerance.
+    assert.strict(
+      0.55 > delayInSeconds && delayInSeconds > 0.5,
+      `unexpected delay: ${delayInSeconds.toFixed(3)} s`
+    );
   });
 
   it('should accept a new axios instance', async () => {
